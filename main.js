cae0f87d129a2cf93df0e35f573a091afcb01ebc
guy <guydovlev@gmail.com>
2022-06-25 17:48:10 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+function drawGrid(){
+    // Create a 16x16 grid with divs
+    var grid = document.getElementById('grid');
+    console.log(grid);
+    for(let i=0; i<16; i++){
+        for(let j=0; j<16; j++){
+            let cell = document.createElement('div');
+            cell.className = "cell";
+            grid.appendChild(cell);
+            
+        }
+    }
+}
+
+drawGrid();
