a36b718c07af1b02049123e8aaaba92f31a3c2eb
guy <guydovlev@gmail.com>
2022-06-28 00:47:43 +0300

    Added dynamic size

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 function drawGrid(){
-    // Create a 16x16 grid with divs
+    // Ask for size
+    var cols = prompt("How many columns do you want?");
+    var rows = prompt("How many rows do you want?");
     var grid = document.getElementById('grid');
-    console.log(grid);
-    for(let i=0; i<16; i++){
-        for(let j=0; j<16; j++){
+
+    for(let i=0; i<cols; i++){
+        for(let j=0; j<rows; j++){
             let cell = document.createElement("div");
-            cell.innerHTML = i+j;
             cell.className = "cell";
             grid.appendChild(cell);
         }
     }
 }
+
+
 
 drawGrid();
