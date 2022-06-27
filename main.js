b3c6daa5c118b73d713ae812c63da5920389daf8
guy <guydovlev@gmail.com>
2022-06-28 00:13:26 +0300

    foundation is done

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ function drawGrid(){
     console.log(grid);
     for(let i=0; i<16; i++){
         for(let j=0; j<16; j++){
-            let cell = document.createElement('div');
+            let cell = document.createElement("div");
+            cell.innerHTML = i+j;
             cell.className = "cell";
             grid.appendChild(cell);
-            
         }
     }
 }
