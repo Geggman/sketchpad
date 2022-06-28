5f05d32e58fb1e410c1da2bae8e58d72ce8735f9
guy <guydovlev@gmail.com>
2022-06-29 00:37:06 +0300

    Added drawing

--- main.js
+++ main.js
@@ -1,22 +1,51 @@
+// Consts
+const DEFAULT_SIZE = 16;
+
+let currentSize = DEFAULT_SIZE;
+
+
+var mouseDown = false;
+document.body.onmousedown = () => { mouseDown = true };
+document.body.onmouseup = () => { mouseDown = false };
+
+var grid = document.getElementById('grid');
+var clearButton = document.getElementById("clear-btn");
+var sizeButton = document.getElementById("size-btn");
+
+// Setup button events
+sizeButton.onclick = resizeGrid;
+clearButton.onclick = clearGrid;
+
+function resizeGrid(){
+    currentSize = prompt("Enter new size");
+    drawGrid(currentSize);
+}
+
 function drawGrid(size){
     // Ask for size
-    var grid = document.getElementById('grid');
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
-    for(let i=0; i<size; i++){
-        for(let j=0; j<size; j++){
-            let cell = document.createElement("div");
-            cell.className = "cell";
-            grid.appendChild(cell);
-        }
+    for(let i=0; i < size*size; i++){
+        let cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.addEventListener('mouseover', changeColor);
+        cell.addEventListener('mousedown', changeColor);
+        grid.appendChild(cell);
     }
 }
 
+function changeColor(e){
+    if(e.type === 'mouseover' && !mouseDown) return;
+    e.target.style.backgroundColor = 'black';
+}
 
-document.getElementById("size-btn").onclick = () => {
-    var size = prompt("How many pixels do you want?");
-    drawGrid(size);
-};
+function clearGrid(){
+    grid.innerHTML = '';
+}
 
+// Start the program
+window.onload = () => {
+    drawGrid(DEFAULT_SIZE);
+    console.log(document.getElementById('grid').classList);
+}
 
-drawGrid();
